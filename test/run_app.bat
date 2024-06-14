0eecd861129a475a90d4db9313fd958ed676e5d8
Claudio_Alcaman <92183693+claudxfiles@users.noreply.github.com>
2024-06-14 18:39:50 -0400

    Add files via upload

---
+++ test/run_app.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Users\calcama\AppData\Local\anaconda3\Scripts\activate.bat" "C:\Users\calcama\AppData\Local\anaconda3"
+
+cd "U:\Users\calcama\Desktop\test\"
+streamlit run setup.py
